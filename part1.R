b99e53c99405125e3169dbfe1642fc3ede9f9423
RadoSlav <radoslav4096@gmail.com>
2023-06-25 00:56:55 +0300

    solved first two tasks from first part

--- part1.R
+++ part1.R
@@ -1,3 +1,19 @@
 # First Part
 
 # Task 1
+sim.5balls <- function(){
+  x <- sample(c(1:5), 2, replace=T)
+  x[1] == x[2]
+}
+
+res <- replicate(100000, sim.5balls())
+sum(res)/length(res)
+
+# Task 2
+sim.socks <- function(){
+  x <- sample(c(1,1,2,2,3,3), 2, replace=F)
+  x[1] == x[2]
+}
+
+res <- replicate(100000, sim.socks())
+sum(res)/length(res)
